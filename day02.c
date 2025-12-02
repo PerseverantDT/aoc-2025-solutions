6d1d68d268dd58497ac58d3d6213a56a88c15543
PerseverantDT <perseverant-determination@outlook.com>
2025-12-02 15:56:36 +0800

    Add solution for day 2 part 2

--- day02.c
+++ day02.c
@@ -8,9 +8,95 @@
 
 FILE *check_file = nullptr;
 
+struct range {
+    uint64_t start;
+    uint64_t end;
+};
+
+struct range get_range(char const *line) {
+    struct range range;
+
+    if (sscanf(line, "%" SCNu64 "-%" SCNu64, &range.start, &range.end) != 2) {
+        fprintf(stderr, "Failed to parse range: \"%s\"", line);
+        range.start = 0;
+        range.end = 0;
+    }
+
+    return range;
+}
+
+[[gnu::nonnull(2), gnu::access(write_only, 2, 3)]]
+static bool convert_number_to_string(
+    uint64_t num,
+    char *buffer,
+    size_t buffer_length
+) {
+    size_t num_of_digits = log10(num) + 1;
+    if (buffer_length < num_of_digits + 1) {
+        return false;
+    }
+
+    for (int index = num_of_digits - 1; num > 0; index--) {
+        uint64_t digit = num % 10;
+        buffer[index] = '0' + digit;
+        num /= 10;
+    }
+    buffer[num_of_digits] = '\0';
+
+    return true;
+}
+
+uint64_t part2_get_subtotal_of_invalid_ids(char const *line) {
+    auto range = get_range(line);
+    char num_as_str[21];
+    char pattern[11];
+    uint64_t subtotal = 0;
+
+    for (auto num = range.start; num <= range.end; num++) {
+        bool is_invalid = false;
+        if (!convert_number_to_string(num, num_as_str, 21)) {
+            fprintf(stderr, "Could not convert %" PRIu64 " into string.\n", num);
+            continue;
+        }
+
+        int num_of_digits = log10(num) + 1;
+        if (num_of_digits == 1) {
+            continue;
+        }
+
+        for (int i = 1; i <= ceilf((float)num_of_digits / 2.0F); i++) {
+            bool check_failed = false;
+            if (num_of_digits % i != 0) {
+                continue;
+            }
+
+            strncpy(pattern, num_as_str, i);
+            pattern[i] = '\0';
+            
+            for (int j = 0; j < num_of_digits / i; j++) {
+                if (strncmp(num_as_str + (i * j), pattern, i) != 0) {
+                    check_failed = true;
+                    break;
+                }
+            }
+
+            if (!check_failed) {
+                is_invalid = true;
+                break;
+            }
+        }
+
+        if (is_invalid) {
+            subtotal += num;
+        }
+    }
+
+    return subtotal;
+}
+
 [[gnu::nonnull(1)]]
 [[gnu::access(read_only, 1)]]
-uint64_t get_subtotal_of_invalid_ids(char const *line) {
+uint64_t part1_get_subtotal_of_invalid_ids(char const *line) {
     uint64_t start;
     uint64_t end;
     uint64_t subtotal = 0;
@@ -67,12 +153,14 @@ int main() {
     char line[42];
     char current_character;
     uint8_t buffer_index = 0;
-    uint64_t invalid_id_sum = 0;
+    uint64_t part1_invalid_id_sum = 0;
+    uint64_t part2_invalid_id_sum = 0;
 
     while (fscanf(input_file, "%c", &current_character) == 1) {
         if (current_character == ',') {
             line[buffer_index] = '\0';
-            invalid_id_sum += get_subtotal_of_invalid_ids(line);
+            part1_invalid_id_sum += part1_get_subtotal_of_invalid_ids(line);
+            part2_invalid_id_sum += part2_get_subtotal_of_invalid_ids(line);
             buffer_index = 0;
         }
         else {
@@ -83,8 +171,10 @@ int main() {
 
     // Process the final range
     line[buffer_index] = '\0';
-    invalid_id_sum += get_subtotal_of_invalid_ids(line);
+    part1_invalid_id_sum += part1_get_subtotal_of_invalid_ids(line);
+    part2_invalid_id_sum += part2_get_subtotal_of_invalid_ids(line);
     buffer_index = 0;
 
-    printf("Part 1: The sum of all invalid IDs is %" PRIu64 "\n", invalid_id_sum);
+    printf("Part 1: The sum of all invalid IDs is %" PRIu64 "\n", part1_invalid_id_sum);
+    printf("Part 2: The sum of all invalid IDs is %" PRIu64 "\n", part2_invalid_id_sum);
 }
