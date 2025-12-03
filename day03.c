0ec03624860c5c89c9db25fe960bd7b4ff59a781
PerseverantDT <perseverant-determination@outlook.com>
2025-12-03 15:18:14 +0800

    day 3: Add solution for part 2

--- day03.c
+++ day03.c
@@ -1,20 +1,22 @@
+#include <alloca.h>
 #include <assert.h>
 #include <errno.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 FILE *check_file = nullptr;
-constexpr int BUFFER_LENGTH = 2048;
+constexpr long BUFFER_LENGTH = 2048;
 
 [[gnu::access(read_only, 1, 2)]]
-int get_highest_joltage(char const *line, int line_length) {
-    int joltage = 0;
-    int joltage2 = 0;
-    int position = -1;
+long part1_get_highest_joltage(char const *line, long line_length) {
+    long joltage = 0;
+    long joltage2 = 0;
+    long position = -1;
 
     // Find the battery with the largest joltage.
-    for (int i = 0; i < line_length; i++) {
+    for (long i = 0; i < line_length; i++) {
         char c = line[i];
         if (c - '0' > joltage) {
             joltage = c - '0';
@@ -28,7 +30,7 @@ int get_highest_joltage(char const *line, int line_length) {
         // The battery with the largest joltage is the last one. The second one
         // needs to be the highest of the others.
 
-        for (int i = 0; i < position; i++) {
+        for (long i = 0; i < position; i++) {
             char c = line[i];
             if (c - '0' > joltage2) {
                 joltage2 = c - '0';
@@ -38,7 +40,7 @@ int get_highest_joltage(char const *line, int line_length) {
         return joltage2 * 10 + joltage;
     }
 
-    for (int i = position + 1; i < line_length; i++) {
+    for (long i = position + 1; i < line_length; i++) {
         char c = line[i];
         if (c - '0' > joltage2) {
             joltage2 = c - '0';
@@ -46,6 +48,39 @@ int get_highest_joltage(char const *line, int line_length) {
     }
 
     return joltage * 10 + joltage2;
+}
+
+[[gnu::access(read_only, 1, 2)]]
+long part2_get_highest_joltage(char const *line, long line_length) {
+    long joltage = 0;
+    long start_index = -1;
+
+    for (int i = 0; i < 12; i++) {
+        long highest_joltage = 0;
+        long highest_joltage_index = -1;
+
+        long search_limit = line_length - (12 - i) + 1;
+        for (long j = start_index + 1; j < search_limit; j++) {
+            long value = line[j] - '0';
+            if (value > highest_joltage) {
+                highest_joltage = value;
+                highest_joltage_index = j;
+            }
+
+            if (value == 9) {
+                // We can't have digits higher than 9, so we stop early.
+                break;
+            }
+        }
+
+        assert(highest_joltage_index != -1);
+
+        start_index = highest_joltage_index;
+        joltage *= 10;
+        joltage += highest_joltage;
+    }
+
+    return joltage;
 }
 
 int main(void) {
@@ -57,10 +92,11 @@ int main(void) {
 
     check_file = fopen("check.txt", "w");
     char buffer[BUFFER_LENGTH];
-    int total_joltage = 0;
+    long part1_total_joltage = 0;
+    long part2_total_joltage = 0;
 
     while (fgets(buffer, BUFFER_LENGTH, input_file) != nullptr) {
-        int line_length = strlen(buffer);
+        long line_length = strlen(buffer);
 
         // Remove trailing new line
         if (buffer[line_length - 1] == '\n' || buffer[line_length - 1] == '\r') {
@@ -72,13 +108,16 @@ int main(void) {
             line_length--;
         }
 
-        int highest_joltage = get_highest_joltage(buffer, line_length);
+        long part1_highest_joltage = part1_get_highest_joltage(buffer, line_length);
+        long part2_highest_joltage = part2_get_highest_joltage(buffer, line_length);
 
+        part1_total_joltage += part1_highest_joltage;
+        part2_total_joltage += part2_highest_joltage;
         if (check_file != nullptr) {
-            fprintf(check_file, "Joltage of \"%s\": %d\n", buffer, highest_joltage);
+            fprintf(check_file, "Joltage of \"%s\":\n\tPart 1: %ld (total: %ld)\n\tPart 2: %ld (total: %ld)\n", buffer, part1_highest_joltage, part1_total_joltage, part2_highest_joltage, part2_total_joltage);
         }
-        total_joltage += highest_joltage;
     }
 
-    printf("Total joltage: %d\n", total_joltage);
+    printf("Part 1: Total joltage: %ld\n", part1_total_joltage);
+    printf("Part 2: Total joltage: %ld\n", part2_total_joltage);
 }
